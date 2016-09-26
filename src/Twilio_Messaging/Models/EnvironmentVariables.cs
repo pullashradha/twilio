14e2c4325b4b93f786bb087dc9d558c6cc8f526b
epicodus <epicodus@DESKTOP-MV40P3S>
2016-09-26 10:29:44 -0700

    Add Message model, views, and HomeController, app & API are working

---
+++ src/Twilio_Messaging/Models/EnvironmentVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Twilio_Messaging.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string AccountSid = "AC6695c0402b6ae65c904ca13458473b23";
+        public static string AuthToken = "a9232c5437a882540bb0dba78fcef9f3";
+    }
+}
